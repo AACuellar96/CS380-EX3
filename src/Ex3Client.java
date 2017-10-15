256cc1bab5a2b485db19a14df537a97b5bba7600
Adrian Cuellar <aacuellar14@gmail.com>
2017-10-15 14:08:16 -0700

    started

---
+++ src/Ex3Client.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.Socket;
+public final class Ex3Client {
+    public static void main(String[] args) throws Exception {
+        try (Socket socket = new Socket("18.221.102.182",38103)) {
+            System.out.println("Connected to server.");
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+            BufferedReader br = new BufferedReader(isr);
+            BufferedReader brIS = new BufferedReader(new InputStreamReader(System.in));
+            PrintStream out = new PrintStream((socket.getOutputStream()),true,"UTF-8");
+            int size = is.read();
+            System.out.print("Reading "+size+" bytes.");
+            byte[] holder = new byte[size];
+            for(int i=0;i<size;i++){
+                holder[i]=(byte) is.read();
+            }
+            int cSum =checksum(holder);
+            String hex = Integer.toHexString(cSum);
+            holder[0] = (byte) Integer.parseInt(hex.substring(0,2).toUpperCase(),16);
+            holder[1] = (byte) Integer.parseInt(hex.substring(2).toUpperCase(),16);
+            out.write(holder);
+            if(is.read()==1)
+                System.out.println("Response good.");
+            else
+                System.out.println("Response false.");
+            is.close();
+            isr.close();
+            br.close();
+            brIS.close();
+            socket.close();
+            System.out.println("Disconnected from server.");
+        }
+    }
+    public static short checksum(byte[] b){
+        short cSum = 0;
+        for(int i=0;i<b.length;i+=2){
+            short one = (short) (b[i] & 0xFF);
+            short two = (short) (b[i+1] & 0xFF);
+            cSum += ((256*one)+ two);
+        }
+        return cSum;
+    }
+}
